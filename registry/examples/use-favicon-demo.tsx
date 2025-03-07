5d163007187ea2098010afe6c7e7e929ee5326ce
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2025-03-08 05:26:39 +0600

    update

--- registry/examples/use-favicon-demo.tsx
+++ registry/examples/use-favicon-demo.tsx
@@ -2,37 +2,11 @@
 
 import * as React from "react"
 import Image from "next/image"
+import { favicons } from "@/utils/favicons"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { useFavicon } from "registry/hooks/use-favicon"
-
-const favicons = [
-  {
-    name: "Toufiq Hasan Kiron",
-    url: "https://toufiqhasankiron.com/favicon.ico",
-    alt: "Toufiq Hasan Kiron logo",
-    title: "Set the favicon to Toufiq Hasan Kiron's logo",
-  },
-  {
-    name: "Next.js",
-    url: "https://nextjs.org/favicon.ico",
-    alt: "Next.js logo",
-    title: "Set the favicon to Next.js's logo",
-  },
-  {
-    name: "React",
-    url: "https://react.dev/favicon.ico",
-    alt: "React logo",
-    title: "Set the favicon to React's logo",
-  },
-  {
-    name: "Tailwind CSS",
-    url: "https://tailwindcss.com/favicon.ico",
-    alt: "Tailwind CSS logo",
-    title: "Set the favicon to Tailwind CSS's logo",
-  },
-]
 
 export default function UseFaviconDemo() {
   const [favicon, setFavicon] = React.useState(favicons[0].url)
