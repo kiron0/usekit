428efc129ec9bf749a1d9ca3b5ed54d2e8ec3cb5
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2026-03-21 23:54:29 +0600

    fix: vercel build issue fixed

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -60,12 +60,23 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ className, orientation, ...props }) => {
+          if (orientation === "left") {
+            return <ChevronLeft className={cn("size-4", className)} {...props} />
+          }
+
+          if (orientation === "right") {
+            return (
+              <ChevronRight className={cn("size-4", className)} {...props} />
+            )
+          }
+
+          if (orientation === "down") {
+            return <ChevronDown className={cn("size-4", className)} {...props} />
+          }
+
+          return <ChevronUp className={cn("size-4", className)} {...props} />
+        },
       }}
       {...props}
     />
